40392bb4aa7768f36cf99dc680e5a8f9a9e8017b
paul.hermann <paul.hermann@stud.tu-darmstadt.de>
2023-06-29 11:46:04 +0200

    Added LAS Reader to evaluation

--- evaluation/src/lib.rs
+++ evaluation/src/lib.rs
@@ -1,9 +1,13 @@
+use std::fs::File;
+use std::io::BufReader;
 use crate::point::{Point, PointIdAttribute};
 use lidarserv_common::geometry::points::PointType;
 use lidarserv_common::geometry::position::I32CoordinateSystem;
 use log::info;
 use std::path::PathBuf;
 use input_file_replay::iter_points::iter_points;
+use lidarserv_common::las::{I32LasReadWrite, Las};
+use lidarserv_server::index::point::LasPoint;
 
 pub mod indexes;
 pub mod insertion_rate;
@@ -17,23 +21,32 @@ pub mod thermal_throttle;
 pub fn read_points(
     coordinate_system: &I32CoordinateSystem,
     settings: &settings::Base,
-) -> Vec<Point> {
+) -> Vec<LasPoint> {
     info!("Reading points...");
-    let points: Vec<_> = iter_points(
-        &settings.trajectory_file,
-        &settings.points_file,
-        settings.offset,
-    )
-    .unwrap()
-    .enumerate()
-    .map(|(id, (_, p))| {
-        let las_point = p.into_las_point(coordinate_system).unwrap();
-        Point {
-            position: las_point.position().clone(),
-            point_id: PointIdAttribute(id),
-        }
-    })
-    .collect();
+
+    let mut points: Vec<LasPoint> = Vec::new();
+
+    // check file format
+    if settings.trajectory_file.extension().unwrap() != "las" {
+        // CSV / TXT Format
+        points = iter_points(
+            &settings.trajectory_file,
+            &settings.points_file,
+            settings.offset,
+        )
+            .unwrap()
+            .enumerate()
+            .map(|(id, (_, p))| {
+                p.into_las_point(coordinate_system).unwrap()
+            })
+            .collect();
+    } else {
+        // LAS Format
+        let f = File::open(&settings.points_file).unwrap();
+        let mut reader = BufReader::new(f);
+        let las_reader : I32LasReadWrite = I32LasReadWrite::new(false, settings.las_point_record_format);
+        let mut result : Las<Vec<LasPoint>> = las_reader.read_las(&mut reader).unwrap();
+    }
     info!("Read a total of {} points.", points.len());
     points
 }
