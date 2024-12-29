a31d118868b204ca3e50a30c81a01f6acad461f9
Lidarserv Evaluation <tobias.dorra@igd.fraunhofer.de>
2024-12-29 15:37:03 +0100

    evaluation results

--- evaluation/2025/article_measurements/run_eval.sh
+++ evaluation/2025/article_measurements/run_eval.sh
@@ -1,37 +1,42 @@
 #!/usr/bin/env bash
 
-DATA="../../../data"
-POTREE="../../../../PotreeConverter/build"
+data="../../../data"
+potree_converter=/home/localadmin/PotreeConverter/build/PotreeConverter
 PG="../pgPointCloud_Measurements/"
 
+# set -o xtrace
+
 # lidarserv measurements
-echo "RUNNING LIDARSERV MEASUREMENTS"
-for input in lille kitti ahn4; do
-#for input in lille; do
- echo Measuring $input
- lidarserv-evaluation $input.toml > lidarserv_$input.out 2>&1
- rm -rf $DATA/$input-eval
-done
+# echo "RUNNING LIDARSERV MEASUREMENTS"
+# for input in lille kitti ahn4; do
+# #for input in lille; do
+#  echo Measuring $input
+#  lidarserv-evaluation $input.toml > lidarserv_$input.out 2>&1
+#  rm -rf $data/${input}-eval
+# done
 
 # potree measurements
 echo "RUNNING POTREE MEASUREMENTS"
 for input in Lille_sorted.las kitti_sorted.las AHN4.las; do
 #for input in Lille_sorted.las; do
  echo Measuring $input
- $POTREE/PotreeConverter $DATA/$input $DATA/${input}_Potree -m poisson --encoding UNCOMPRESSED > ${input}_uncompressed.out
- du -s $DATA/${input}_converted >> ${input}_uncompressed.out
- rm -rf $DATA/${input}_converted
- $POTREE/PotreeConverter $DATA/$input $DATA/${input}_Potree -m poisson --encoding BROTLI > ${input}_compressed.out
- du -s $DATA/${input}_converted >> ${input}_compressed.out
- rm -rf $DATA/${input}_converted
+
+ $potree_converter $data/$input -o $data/${input}_Potree -m poisson --encoding UNCOMPRESSED > ${input}_uncompressed.out
+ du -s $data/${input}_Potree >> ${input}_uncompressed.out
+ rm -rf $data/${input}_Potree
+
+ $potree_converter $data/$input -o $data/${input}_Potree -m poisson --encoding BROTLI > ${input}_compressed.out
+ du -s $data/${input}_Potree >> ${input}_compressed.out
+ rm -rf $data/${input}_Potree
 done
 
 # pgpointcloud measurements
-echo "RUNNING PGPOINTCLOUD MEASUREMENTS"
-cd ../pgPointCloud_Measurements/
-for input in Lille_sorted.las kitti_sorted.las AHN4.las; do
-#for input in Lille_sorted.las; do
- echo Measuring $input
- cargo run --release --bin insertion -- --input-file $DATA/$input --compression none
- cargo run --release --bin query -- --input-file $DATA/$input --drop-table
-done
+# echo "RUNNING PGPOINTCLOUD MEASUREMENTS"
+# cd ../pgPointCloud_Measurements/
+# for input in Lille_sorted.las kitti_sorted.las AHN4.las; do
+# #for input in Lille_sorted.las; do
+#  echo Measuring $input
+#  cargo run --release --bin insertion -- --input-file $DATA/$input --compression none
+#  cargo run --release --bin query -- --input-file $DATA/$input --drop-table
+# done
+
